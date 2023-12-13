0021790b26dee9be2bc3d7fed72b52b73ffe2acd
Sebastian Erfort <s.erfort@rug.nl>
2023-12-13 14:29:21 +0100

    add key map <leader>+zz for true-zen focus

--- lua/custom/plugins/editing.lua
+++ lua/custom/plugins/editing.lua
@@ -1,4 +1,8 @@
 return {
-  { "Pocco81/true-zen.nvim", opts = {} },
+  { "Pocco81/true-zen.nvim",
+    opts = {
+    vim.api.nvim_set_keymap("n", "<leader>zz", ":TZFocus<CR>", {})
+    }
+  },
   { 'godlygeek/tabular' },
 }
